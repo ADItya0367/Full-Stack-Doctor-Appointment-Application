aae0846aa3a05130a9b12f9fa41575ece31798d8
ADItya0367 <21egjcs001@gitjaipur.com>
2024-05-24 19:20:19 +0530

    Libraries and Routes Setup

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
-const express = require('express')
+const express = require('express');
+const { default: Login } = require('./client/src/pages/Login');
 const app = express();
 require('dotenv').config()
 const port = process.env.PORT || 3000
 
+app.get('/login', (req, res) => {
+    <Login/>
+})
+
 app.listen(port, () => {
-    console.log('Someone is Listning to us on port 3000')
+    console.log('Someone is Listning to us on port ',port)
 })
