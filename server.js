b4c6eb37124a894d2e1d65396a108a23efd0a8f1
nilpeep <niluferk038@gmail.com>
2024-05-25 12:59:13 +0300

    up

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { default: Login } = require('./client/src/pages/Login');
 const app = express();
 require('dotenv').config()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 
 app.get('/login', (req, res) => {
     <Login/>
