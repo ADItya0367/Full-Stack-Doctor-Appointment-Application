3d5a0c1aded52c4d9090b14bf1fc5b4e0362cf5f
nilpeep <niluferk038@gmail.com>
2024-05-25 13:09:21 +0300

    up

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const express = require('express');
-const { default: Login } = require('./client/src/pages/Login');
 const app = express();
 require('dotenv').config()
 const port = process.env.PORT || 4000
 
-app.get('/login', (req, res) => {
-    <Login/>
-})
+
 
 app.listen(port, () => {
     console.log('Someone is Listning to us on port ',port)
