3ddf4986c99d1be9d3a197c3090aba556503d25e
ADItya0367 <21egjcs001@gitjaipur.com>
2024-05-25 13:06:46 +0530

    Backend setup , Installed Libraries

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
-const { default: Login } = require('./client/src/pages/Login');
-const app = express();
+const mongoose = require('mongoose')
+const cors = require('cors')
 require('dotenv').config()
 const port = process.env.PORT || 3000
+const app = express();
 
-app.get('/login', (req, res) => {
-    <Login/>
-})
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect('mongodb+srv://Aditya:abcde12345@nodejs.euh2mao.mongodb.net/?retryWrites=true&w=majority&appName=NodeJs').then((d)=>{
+    console.log('Connected to MongoDB')
+
+}).catch(err => console.log(err))
 
 app.listen(port, () => {
     console.log('Someone is Listning to us on port ',port)
